dddb7e1f40e6bc19da5feb0e4791def3f9572118
EzHawkeye <66911686+EzHawkeye@users.noreply.github.com>
2021-07-22 16:37:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const botConfig = require("./botconfig.json");
 
-const SUGGESTION_CHANNEL = '86284582324276190'
+const SUGGESTION_CHANNEL = '862845823242076190'
 const SUGGESTION_CHANNEL2 = '746895559201259663'
 
 Discord.RichEmbed = Discord.MessageEmbed;
