d1a11575b9eac12d4d0ab9664fdbeb3151f7ef3e
EzHawkeye <66911686+EzHawkeye@users.noreply.github.com>
2021-07-23 18:44:58 +0200

    gg

---
+++ commands/nuke.js
@@ -0,0 +1,23 @@
+const discord = require("discord.js");
+
+module.exports = {
+    description: "Nukes a given channel",
+    authorPermission: "ADMINISTRATOR",
+    run: async(client, message, args) => {
+        let reason = args.join(" ") || "No Reason"
+        if(!message.channel.deletable) {
+            return message.reply("This channel cannot be nuked!")
+        }
+        let newchannel = await message.channel.clone()
+        await message.channel.delete()
+        let embed = new MessageEmbed()
+        .setTitle("Channel Nuked")
+        .setDescription(reason)
+        .setImage('https://media0.giphy.com/media/oe33xf3B50fsc/200.gif')
+        await newchannel.send(embed)
+    }
+}
+
+module.exports.help = {
+    name: "nuke"
+}
